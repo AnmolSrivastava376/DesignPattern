4564ca4362a39e7c94a1484c0717fbd0aecd811b
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-03-24 23:30:08 +0530

    singleton

---
+++ SingletonMethod/src/SingletonExample.java
@@ -0,0 +1,6 @@
+public class SingletonExample {
+    public static void main(String[] args) {
+        Singleton singletonInstance = Singleton.getInstance();
+        singletonInstance.showMessage();
+    }
+}
